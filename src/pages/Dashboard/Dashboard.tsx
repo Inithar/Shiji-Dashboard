e08b148fab479609dec68e2bf1a98ef4a6158742
Szymon Świtała <szymon.switala@proton.me>
2025-03-18 15:50:45 +0100

    feat: add JSON server and implement data fetching

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 import Header from "../../components/Header/Header.tsx";
 import ReservationBoard from "../../components/ReservationsDashboard/ReservationBoard.tsx";
 
+import useQuery from "../../hooks/useQuery.ts";
 import { mapResponseObjectToReservation } from "../../utils/reservationUtils.ts";
 import { Reservation } from "../../types/reservation.ts";
-import reservationsData from "../../data/reservations.json";
 
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  const [reservations, setReservations] = useState<Reservation[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { data: reservations, loading, error } = useQuery<Reservation[]>("http://localhost:3000/reservations");
 
-  useEffect(() => {
-    setTimeout(() => {
-      try {
-        const validReservations = reservationsData.map(mapResponseObjectToReservation);
-        setReservations(validReservations);
-      } catch (error) {
-        console.error("Błąd podczas przetwarzania danych rezerwacji:", error);
-      } finally {
-        setLoading(false);
-      }
-    }, 800);
-  }, []);
+  const mappedReservations = useMemo(() => {
+    if (!reservations) {
+      return null;
+    }
+
+    try {
+      return reservations.map(mapResponseObjectToReservation);
+    } catch (error) {
+      console.log(`Błąd podczas przetwarzania danych rezerwacji: ${error}`);
+      return null;
+    }
+  }, [reservations]);
+
+  if (loading) {
+    return <div className={styles.loading}>Ładowanie danych rezerwacji...</div>;
+  }
+
+  if (error || !mappedReservations) {
+    return <div>Wystąpił nieoczekiwany błąd. Spróbuj ponownie później.</div>;
+  }
 
   return (
     <div className={styles.container}>
       <Header />
       <main className={styles.mainContent}>
-        {loading ? (
-          <div className={styles.loading}>Ładowanie danych rezerwacji...</div>
-        ) : (
-          <ReservationBoard reservations={reservations} />
-        )}
+        <ReservationBoard reservations={mappedReservations} />
       </main>
     </div>
   );
